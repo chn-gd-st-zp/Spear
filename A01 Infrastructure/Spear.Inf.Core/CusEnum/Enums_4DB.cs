feabfdb959de7e622de536b7b60d51258ffc7c80
zp <13417068686@163.com>
2021-05-07 04:22:50 +0800

    补充注释

--- A01 Infrastructure/Spear.Inf.Core/CusEnum/Enums_4DB.cs
+++ A01 Infrastructure/Spear.Inf.Core/CusEnum/Enums_4DB.cs
@@ -1,9 +1,17 @@
-﻿namespace Spear.Inf.Core.CusEnum
+﻿using Spear.Inf.Core.Attr;
+
+namespace Spear.Inf.Core.CusEnum
 {
     public enum Enum_DBType
     {
+
+        [Remark("默认、无")]
         None,
+
+        [Remark("EF")]
         EF,
+
+        [Remark("SqlSugar")]
         SqlSugar,
     }
 }
