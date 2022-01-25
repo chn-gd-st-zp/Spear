c511884452f5f145dfb1d58b0c3bbe2817a44b32
Bruno Zhang <13417068686@163.com>
2022-01-25 21:18:53 +0800

    new jsonconvert for datetime

---
+++ A01 Infrastructure/Spear.Inf.Core/Tool/Json/JsonDateTimeConverter.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json.Converters;
+
+namespace Spear.Inf.Core.Tool
+{
+    public class JsonDateTimeConverter : IsoDateTimeConverter
+    {
+        public JsonDateTimeConverter(string format) { DateTimeFormat = format; }
+    }
+}
